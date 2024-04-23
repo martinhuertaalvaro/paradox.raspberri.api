5a1590e049bade25d712303c40e840fead724b01
Alvaro <martinhuertaalvaro@gmail.com>
2024-04-23 17:08:16 +0200

    add routes

--- server.py
+++ server.py
@@ -1,7 +1,10 @@
-from flask import Flask, jsonify
+from flask import Flask,jsonify
 import random
+from flask_cors import CORS
+
 
 app = Flask(__name__)
+CORS(app, origins=['http://localhost:4200'])
 
 @app.route('/')
 def index():
